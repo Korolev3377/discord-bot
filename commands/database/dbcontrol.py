bd9db20280a89ac03ff72bf60579f0a183153d89
Korolev3377 <pingdowner@gmail.com>
2023-06-13 16:18:37 +0400

    begin db creation

---
+++ commands/database/dbcontrol.py
@@ -0,0 +1,17 @@
+def connect(): pass
+
+
+def disconnect(): pass
+
+
+def add_user_money(): pass
+
+
+def sub_user_money(): pass
+
+
+def set_user_money(): pass
+
+
+def transaction_between_users(): pass
+
--- commands/main.py
+++ commands/main.py
@@ -1,7 +1,8 @@
 from .regular.main import facts, cults, rolldice
 from .enjoy.main import enjoy_grp
+from .database.main import wealthgrp
 
-commands_to_declare = [facts, cults, rolldice, enjoy_grp]
+commands_to_declare = [facts, cults, rolldice, enjoy_grp, wealthgrp]
 # commands_to_declare = [cults]
 
 
