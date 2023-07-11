3782c8333dd9781d1cded551ffa2073bf25bcacb
Korolev3377 <pingdowner@gmail.com>
2023-07-11 10:10:28 +0400

    db control remove users from class and add fn hints

--- commands/database/dbcontrol.py
+++ commands/database/dbcontrol.py
@@ -1,5 +1,5 @@
 import sqlite3 as sql
-from typing import Any, Union
+from typing import Any, Union, NoReturn
 
 from discord.app_commands import Choice
 
@@ -7,27 +7,17 @@ from environment.variable import *
 
 
 class DataBase:
-    def __init__(self):
+    def __init__(self) -> None:
         self.db_connection = None
         self.db_cursor = None
-        self.users = None
         self.connect()
         self.disconnect()
 
-    def connect(self):
+    def connect(self) -> NoReturn:
         self.db_connection = sql.connect("database.db")
         self.db_cursor = self.db_connection.cursor()
-        self.db_cursor.execute("SELECT name, id FROM users WHERE is_visible = 1;")
-        i = self.db_cursor.fetchall()
-        dat = []
-        if i:
-            for _ in i:
-                dat.append(Choice(name=str(_[0]), value=str(_[1])))
-            self.users = dat
-        else:
-            self.users = None
 
-    def disconnect(self):
+    def disconnect(self) -> NoReturn:
         self.db_connection.commit()
         self.db_connection.close()
 
