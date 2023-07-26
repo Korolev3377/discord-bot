dad4943af698ccdb32e1d7e59f29c0d1db36a5b2
Korolev3377 <pingdowner@gmail.com>
2023-07-26 09:53:04 +0400

    Dice modifications

--- commands/regular/main.py
+++ commands/regular/main.py
@@ -11,6 +11,7 @@ from environment.variable import *
 from environment.facts import Facts
 from commands.database.dbcontrol import DB
 
+
 _F = Facts()
 
 
@@ -18,54 +19,57 @@ def capitalize_words(string):
     return str.join(' ', [word.capitalize() for word in string.split(' ')])
 
 
-FACT_NAME = "fact_name"
-FACT_DESC = "facts_desc"
-NO_FACTS = "no_facts"
+FACT_CMD_NAME = "fact_cmd_name"
+FACT_CMD_DESC = "fact_cmd_desc"
+NO_FACTS_IN_DB = "no_facts_in_db"
 
-CULTS_NAME = "cults_name"
-CULTS_DESC = "cults_desc"
-TOP_CULT = "top_cults"
-CULT_INFO = "members_count"
+CULTS_CMD_NAME = "cults_cmd_name"
+CULTS_CMD_DESC = "cults_cmd_desc"
+TOP_CULTS = "top_cults"
+MEMBERS_IN_CULT = "members_in_cult"
 NO_CULTS = "no_cults"
+BY_WEALTH = "by_wealth"
+BY_MEMBERS = "by_members"
+SORT = "sort"
 
-ROLLDICE_NAME = "rd_name"
-ROLLDICE_DESC = "rd_desc"
-TOO_MANY_DICES = "tmd"
-ZERO_OUTPUT = "zo"
+ROLLDICE_CMD_NAME = "rolldice_cmd_name"
+ROLLDICE_CMD_DESC = "rolldice_cmd_desc"
+TOO_MANY_DICE = "too_many_dice"
+NOTHING_TO_ROLL = "nothing_to_roll"
 
 _locale = {
-    FACT_NAME: {EN: "fun-fact",
-                RU: "забавный-факт"},
-    FACT_DESC: {EN: "Let me gave u funny fackt.",
-                RU: "Дай мне дать тебе забавный факт."},
-    NO_FACTS: {EN: "Sorry, no facts on this server",
-               RU: "Сорри, нету фактов на этом сервере."},
+    FACT_CMD_NAME: {EN: "fun-fact",
+                    RU: "забавный-факт"},
+    FACT_CMD_DESC: {EN: "Learn a fun fact",
+                    RU: "Узнать забавный факт"},
+    NO_FACTS_IN_DB: {EN: "Sorry... I don't have any facts yet! :(",
+                     RU: "Простите... У меня пока что нету фактов! :("},
 
-    CULTS_NAME: {EN: "top-cults",
-                 RU: "топ-культов"},
-    CULTS_DESC: {EN: "Check most popular cults on this server.",
-                 RU: "Проверить наиболее популярные культы на этом сервере."},
-    TOP_CULT: {EN: "Top {_} cults",
-               RU: "Топ {_} культов"},
-    CULT_INFO: {EN: "Members: {_}. Wealth: {_1}.",
-                RU: "Участников: {_}. Богатство: {_1}."},
+    CULTS_CMD_NAME: {EN: "cults",
+                     RU: "культы"},
+    CULTS_CMD_DESC: {EN: "Show server cults",
+                     RU: "Показать культы сервера"},
+    TOP_CULTS: {EN: "Top cults",
+                RU: "Топ культов"},
+    MEMBERS_IN_CULT: {EN: "Members: {_} - Wealth: {_1}.",
+                      RU: "Участников: {_} - Богатство: {_1}."},  #
     NO_CULTS: {EN: "Sorry, no cults on this server.",
                RU: "Извините, на этом сервере нет культов."},
 
-    ROLLDICE_NAME: {EN: "roll-dice",
-                    RU: "кинуть-кубики"},
-    ROLLDICE_DESC: {EN: "Trhow some dises.",
-                    RU: "Книуть немного кубеков."},
-    TOO_MANY_DICES: {EN: "Too many dices. Maxium == 10.",
-                     RU: "Слишком много кубиков. Максимум - 10."},
-    ZERO_OUTPUT: {EN: "No info for output. Check input.",
-                  RU: "Нет информации для вывода. Проверте ввод."},
-    "sort_by": {EN: "sort",
-                RU: "сортировать"},
-    "by_members": {EN: "By members",
-                   RU: "По участникам"},
-    "by_wealth": {EN: "By wealth",
-                  RU: "По богатству"}
+    ROLLDICE_CMD_NAME: {EN: "roll-dice",
+                        RU: "кинуть-игральные-кости"},
+    ROLLDICE_CMD_DESC: {EN: "Randomizer",
+                        RU: "Рандомайзер"},
+    TOO_MANY_DICE: {EN: "Too many dice. Maximum is 10",
+                    RU: "Слишком много игральных костей. Максимум - 10"},
+    NOTHING_TO_ROLL: {EN: "Nothing to roll",
+                      RU: "Нечего кидать"},  #
+    SORT: {EN: "sort",
+           RU: "сортировать"},
+    BY_MEMBERS: {EN: "By members",
+                 RU: "По участникам"},
+    BY_WEALTH: {EN: "By wealth",
+                RU: "По богатству"}
 }
 
 _T = T(locale_dict=_locale)
@@ -75,8 +79,8 @@ _T = T(locale_dict=_locale)
 
 
 @app_commands.command(
-    name=namedesc(FACT_NAME, _locale),
-    description=namedesc(FACT_DESC, _locale),
+    name=namedesc(FACT_CMD_NAME, _locale),
+    description=namedesc(FACT_CMD_DESC, _locale),
     extras={IS_BROKEN: False}
 )
 async def facts(interaction: discord.Interaction):
@@ -85,17 +89,17 @@ async def facts(interaction: discord.Interaction):
     if fact := await _F.read_facts(guild=interaction.guild, lang=_T.get_lang(interaction.locale.value)):
         await interaction.followup.send(fact)
     else:
-        await interaction.followup.send(_T.stranslate(_ls(NO_FACTS)))
+        await interaction.followup.send(_T.stranslate(_ls(NO_FACTS_IN_DB)))
 
 
 @app_commands.command(
-    name=namedesc(CULTS_NAME, _locale),
-    description=namedesc(CULTS_DESC, _locale)
+    name=namedesc(CULTS_CMD_NAME, _locale),
+    description=namedesc(CULTS_CMD_DESC, _locale)
 )
-@app_commands.rename(sort_by=namedesc("sort_by", _locale))
+@app_commands.rename(sort_by=namedesc(SORT, _locale))
 @app_commands.choices(sort_by=[
-    app_commands.Choice(name=namedesc("by_members", _locale), value=0),
-    app_commands.Choice(name=namedesc("by_wealth", _locale), value=1)])
+    app_commands.Choice(name=namedesc(BY_MEMBERS, _locale), value=0),
+    app_commands.Choice(name=namedesc(BY_WEALTH, _locale), value=1)])
 async def cults(interaction: discord.Interaction, sort_by: app_commands.Choice[int] = 0):
     await interaction.response.defer()
     _T.set_language(interaction.locale)
@@ -151,7 +155,7 @@ async def cults(interaction: discord.Interaction, sort_by: app_commands.Choice[i
         embed = discord.Embed(
             title=_T.stranslate(
                 _ls(
-                    TOP_CULT,
+                    TOP_CULTS,
                     extras={
                         FORMAT: {"_": len(cults_tuple)}
                     }
@@ -164,7 +168,7 @@ async def cults(interaction: discord.Interaction, sort_by: app_commands.Choice[i
                     name=f"{i + 1}) {capitalize_words(cult_name)}",
                     value=_T.stranslate(
                         _ls(
-                            CULT_INFO,
+                            MEMBERS_IN_CULT,
                             extras={
                                 FORMAT: {"_": clist.get(cult_name),
                                          "_1": cmoney.get(cult_name)}
@@ -179,30 +183,53 @@ async def cults(interaction: discord.Interaction, sort_by: app_commands.Choice[i
 
 
 @app_commands.command(
-    name=namedesc(ROLLDICE_NAME, _locale),
-    description=namedesc(ROLLDICE_DESC, _locale)
+    name=namedesc(ROLLDICE_CMD_NAME, _locale),
+    description=namedesc(ROLLDICE_CMD_DESC, _locale)
 )
 async def rolldice(interaction: discord.Interaction, *, dice_args: str):
     await interaction.response.defer()
     _T.set_language(interaction.locale)
     results = {}
-    dices = re.findall(r'\d*[dк-]\d+', dice_args)
+    dices = re.findall(r'(\d*)([dк])(\d+)([+-\\*/]?)(\d+)?', dice_args)
     if len(dices) > 10:
-        await interaction.followup.send(_T.stranslate(_ls(TOO_MANY_DICES)))
+        await interaction.followup.send(_T.stranslate(_ls(TOO_MANY_DICE)))
         return
     for dice in dices:
-        count, value = re.split(r'[dк-]', dice)
+        count, spliter, value, op, num = dice
         result = []
         if count != '':
             if not (0 < int(count) < 11) or not (1 < int(value) < 101):
                 continue
             for i in range(1, int(count) + 1):
-                result.append(random.randint(1, int(value)))
+                res = random.randint(1, int(value))
+                if num:
+                    if op == "+":
+                        res += int(num)
+                    elif op == "-":
+                        res -= int(num)
+                    elif op == "*":
+                        res *= int(num)
+                    elif op == "/":
+                        res = res/int(num)
+                result.append(res)
         else:
             if not (1 < int(value) < 101):
                 continue
-            result.append(random.randint(1, int(value)))
-        results[f'{dice}'] = result
+            res = random.randint(1, int(value))
+            if num:
+                if op == "+":
+                    res += int(num)
+                elif op == "-":
+                    res -= int(num)
+                elif op == "*":
+                    res *= int(num)
+                elif op == "/":
+                    if int(num)>0:
+                        res = res / int(num)
+                    else:
+                        res = 0
+            result.append(res)
+        results[f'{count}{spliter}{value}{op}{num}'] = result
     tosay = ''
     gsum = 0
     for r in results.items():
@@ -217,6 +244,6 @@ async def rolldice(interaction: discord.Interaction, *, dice_args: str):
     if len(results.items()) > 1:
         tosay += f'{gsum}'
     if tosay == '':
-        await interaction.followup.send(_T.stranslate(_ls(ZERO_OUTPUT)))
+        await interaction.followup.send(_T.stranslate(_ls(NOTHING_TO_ROLL)))
         return
     await interaction.followup.send(tosay)
