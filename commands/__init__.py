948738b5a4eccf93d0123e4a8c8b0587f3c5352a
Korolev3377 <pingdowner@gmail.com>
2024-10-31 14:24:12 +0400

    31.10.2024

--- commands/__init__.py
+++ commands/__init__.py
@@ -21,20 +21,24 @@ COMMANDS_DICT = {
 async def declare_commands(bot):
     bot.logger.info("Запущено обновление команд.")
     async for g in bot.fetch_guilds():  # Загрузка конфига комманд для каждого сервера. И настройка конфигов пересылки сообщений...
-        bot.logger.info(["g", g.id])
-        status_d2t_0, _ = check_config(bot.guilds_data, [str(g.id), "discord2tg_bridge", "enable_d2t"])
-        status_d2t_1, _ = check_config(bot.guilds_data, [str(g.id), "discord2tg_bridge", "enable_from_tg"])
-        status_d2t_2, _ = check_config(bot.guilds_data, [str(g.id), "discord2tg_bridge", "from_tg"])
-        if status_d2t_0 and status_d2t_1 and status_d2t_2:
-            if bot.guilds_data.get(str(g.id)).get("discord2tg_bridge", "enable_d2t"):
-                if bot.guilds_data.get(str(g.id)).get("discord2tg_bridge", "enable_from_tg"):
-                    mfilter = bot.guilds_data.get(str(g.id)).get("discord2tg_bridge").get("from_tg").split(" ")
-                    # mfilter == ["1090104010005050103:-1000202090908+2060", "1090104010005050103:-1000202090908+2060"]
-                    for mf in mfilter:
-                        bot.guilds_data[str(mf.split(":")[1].split("+")[0])] = {str(mf.split(":")[1].split("+")[1]): str(mf.split(":")[0])}
+        try:
+            bot.logger.info(["g", g.id])
+            status_d2t_0, _ = check_config(bot.guilds_data, [str(g.id), "discord2tg_bridge", "enable_d2t"])
+            status_d2t_1, _ = check_config(bot.guilds_data, [str(g.id), "discord2tg_bridge", "enable_from_tg"])
+            status_d2t_2, _ = check_config(bot.guilds_data, [str(g.id), "discord2tg_bridge", "from_tg"])
+            if status_d2t_0 and status_d2t_1 and status_d2t_2:
+                if bot.guilds_data.get(str(g.id)).get("discord2tg_bridge", "enable_d2t"):
+                    if bot.guilds_data.get(str(g.id)).get("discord2tg_bridge", "enable_from_tg"):
+                        mfilter = bot.guilds_data.get(str(g.id)).get("discord2tg_bridge").get("from_tg").split(" ")
+                        # mfilter == ["1090104010005050103:-1000202090908+2060", "1090104010005050103:-1000202090908+2060"]
+                        for mf in mfilter:
+                            bot.logger.info([mf])
+                            bot.guilds_data[str(mf.split(":")[1].split("+")[0])] = {str(mf.split(":")[1].split("+")[1]): str(mf.split(":")[0])}
 
-        bot.tree.clear_commands(guild=g)
-        bot.tree.add_command(admingrp, guild=g)
+            bot.tree.clear_commands(guild=g)
+            bot.tree.add_command(admingrp, guild=g)
+        except: pass
+
         for k, v in COMMANDS_DICT.items():
             status, code = check_config(bot.guilds_data, [str(g.id), "commands_to_declare", k])
             codes = {
