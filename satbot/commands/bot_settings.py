b5c778d3463d700ea5c10b27bda8dce786dcff35
Korolev3377 <pingdowner@gmail.com>
2026-05-15 09:23:39 +0400

    bot_settings checks.has_permissions

--- satbot/commands/bot_settings.py
+++ satbot/commands/bot_settings.py
@@ -10,6 +10,15 @@ Log = logging.getLogger(__name__)
     name="bot_settings",
     description="Настройка бота"
 )
-async def configembedcmd(interaction: discord.Interaction):
+@discord.app_commands.checks.has_permissions(administrator=True)
+async def command(interaction: discord.Interaction):
     await interaction.response.defer(thinking=True, ephemeral=True)
     await interaction.followup.send("WIP")
+
+@command.error
+async def error(interaction: discord.Interaction, e: discord.app_commands.AppCommandError):
+    await interaction.response.defer(thinking=True, ephemeral=True)
+    if isinstance(e, discord.app_commands.MissingPermissions):
+        await interaction.followup.send(f"Clearance insufficient")
+    else:
+        await interaction.followup.send(f"Unhandled error: {str(e)}")
